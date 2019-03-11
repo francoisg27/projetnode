ad933642256c52b0f3761cddfa99070722721701
Christian Letembet <claudechristian.letembetambily@ynov.com>
2019-03-11 15:02:01 +0100

    Création de branche Christian

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const {mongoose} = require ('./db/mongoose');
 const argv = require('yargs').argv;
 const http = require('http');
 const app = express();
+const inputPort = argv.port;
+
+const {Client} = require ('./models/client');
+
+const {Car} = require ('./models/car');
+
+const {Seller} = require ('./models/seller');
 
 app.use(bodyParser.json())
+
+
 
 
 app.listen(3000, ()=> console.log('Listening on port 3000'));
