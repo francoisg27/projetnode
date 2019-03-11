8826b1ff3102e59f4ddf2d035e69b86868fd8c7b
francois girard <francoisgirard43@gmail.com>
2019-03-11 11:39:34 +0100

    modele client

---
+++ models/client.js
@@ -0,0 +1,25 @@
+const {mongoose} = require('../db/mongoose.js');
+
+const Client = mongoose.model('Client',{
+    firstName:{
+        type:String,
+        required: true,
+        minlength:1
+    },
+    lastName:{
+        type:String,
+        required: true,
+        minlength:1
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true
+    }
+
+})
+
+module.exports =  { Client };
