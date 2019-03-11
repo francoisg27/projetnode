2a61ea8f1c14ae56503ca1d40b6921117d6eb5b2
francois girard <francoisgirard43@gmail.com>
2019-03-11 16:20:25 +0100

    ajout get-post client

--- server.js
+++ server.js
@@ -7,12 +7,39 @@ const app = express();
 
 app.use(bodyParser.json())
 
+const {Client} = require ('./models/client');
+
+const {Car} = require ('./models/car');
+
+const {Seller} = require ('./models/seller');
 
 
 
+// GET CLIENT METHOD
+app.get('/get/client',function(req,res){
+    Client.find(function(err,client){
+        if(err){
+            res.send(err);
+        }
+        res.send(client);
+    });
+})
 
+//POST CLIENT METHOD
+app.post('/post/client',function(req,res){
+    var client = new Client();
+    client.firstName = req.body.firstName;
+    client.lastName = req.body.lastName;
+    client.age = req.body.age;
+    client.phoneNumber = req.body.phoneNumber;
+    client.save(function(err){
+        if(err){
+            res.send(err);
+        }
+        res.send({message: 'client created'});
+    })
 
-
+})
 
 
 
